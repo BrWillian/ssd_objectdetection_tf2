7c86d477ad29673841353f6499b74dbb2509ce87
Willian Antunes <wiliam-m-@hotmail.com>
2021-07-20 00:26:53 -0300

    updated vgg16 model

--- model/base_networks/vgg16_notop.py
+++ model/base_networks/vgg16_notop.py
@@ -6,10 +6,11 @@ weights_notop_url = ('https://storage.googleapis.com/tensorflow/'
                      'keras-applications/vgg16/'
                      'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
 
-class VGG16:
-    def __init__(self, input_shape=None,
-                 kernel_initializer=None,
-                 kernel_regularizer=None):
+def VGG16(
+        input_shape=None,
+        kernel_initializer=None,
+        kernel_regularizer=None,
+):
         input_layer = Input(shape=input_shape, name="input")
 
         #block1
@@ -27,3 +28,40 @@ class VGG16:
         pool2 = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool', padding='same')(conv2_2)
 
         #block3
+        conv3_1 = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(pool2)
+        conv3_2 = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(conv3_1)
+        conv3_3 = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(conv3_2)
+        pool3 = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool', padding='same')(conv3_3)
+
+        #block4
+        conv4_1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(pool3)
+        conv4_2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(conv4_1)
+        conv4_3 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(conv4_2)
+        pool4 = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool', padding='same')(conv4_3)
+
+        #block5
+        conv5_1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(pool4)
+        conv5_2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(conv5_1)
+        conv5_3 = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3',
+                         kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer)(conv5_2)
+        pool5 = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool', padding='same')(conv5_3)
+
+        model = Model(inputs=input_layer, outputs=pool5)
+
+        weights_path = data_utils.get_file(
+            'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
+            'https://storage.googleapis.com/tensorflow/keras-applications/vgg16/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
+            cache_subdir='models',
+            file_hash='6d6bbae143d832006294945121d1f1fc'
+        )
+        model.load_weights(weights_path, by_name=True)
+
+        return model
